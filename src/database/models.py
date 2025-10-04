26f50600649b0d8c3df2470a9e07fb87ab74e1b3
PRANAY <84889578+PRONOI@users.noreply.github.com>
2025-10-04 12:03:28 +0530

    Update models.py

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient, ASCENDING, DESCENDING
+from pymongo import MongoClient, ASCENDING, DESCENDING, TEXT
 from config.settings import MONGODB_URI
 
 client = MongoClient(MONGODB_URI)
@@ -8,4 +8,6 @@ news = db.news
 def create_indexes():
     news.create_index("url", unique=True)
     news.create_index([("created_at", DESCENDING)])
-  
+    # text-search for /players <name>
+    news.create_index([("text", TEXT)])
+    
