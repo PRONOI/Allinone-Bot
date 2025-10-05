e50a233f0c5f12d8b339ecf6f4f2cf6b34bff492
PRANAY <84889578+PRONOI@users.noreply.github.com>
2025-10-05 10:45:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # 1. start the bot in the background
-python3 bot.py &
+python src/main.py &
 
 # 2. start the web server in the foreground (so Render sees the port)
 exec gunicorn app:app -b 0.0.0.0:$PORT
