7d2c40ebde2ae3e0d49b437e4cc8535c00fbcef3
PRANAY <84889578+PRONOI@users.noreply.github.com>
2025-10-05 14:10:25 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# 1. start the bot in the background
-python src/main.py &
-
-# 2. start the web server in the foreground (so Render sees the port)
-exec gunicorn app:app -b 0.0.0.0:$PORT
